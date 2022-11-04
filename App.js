497e86bed4df9060fd7cdf465484c7b863e08abc
nanditha98 <102796257+nanditha98@users.noreply.github.com>
2022-11-04 07:32:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Horizantalchart from './Chart-Horizantal';
+
+function App() {
+  return (
+    <div className="App">
+    <Horizantalchart/>
+    </div>
+  );
+}
+
+export default App;
