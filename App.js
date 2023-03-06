07b5ee5b9cc794a0cb5571e1d8e2e5c430f0cb3e
nanditha98 <102796257+nanditha98@users.noreply.github.com>
2023-03-06 07:33:34 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-
+// import logo from './logo.svg';
 import './App.css';
-import Horizantalchart from './Chart-Horizantal';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import Home from './Components/Home/Home';
+import Protected from "./Components/Protected/Protected"
+
 
 function App() {
   return (
     <div className="App">
-    <Horizantalchart/>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+     <BrowserRouter>
+<Routes>
+<Route path="/" element={<Login />} />
+<Route
+path="/home"
+element={
+<Protected>
+  <Home />
+</Protected>
+}
+/>
+</Routes>
+</BrowserRouter>
+     
     </div>
   );
 }
---
+++ Home.js
@@ -0,0 +1,13 @@
+import Navbar from "../Navbar/Navbar";
+import React from "react";
+
+function Home (){
+    
+    return(
+<div>
+  
+<Navbar/>
+</div>
+    )
+}
+export default Home
