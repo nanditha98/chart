ce0c8eaba00669a74ca1e297666b8f3523c769ba
nanditha98 <102796257+nanditha98@users.noreply.github.com>
2023-03-10 08:01:33 +0530

    Add files via upload

--- Home.js
+++ Home.js
@@ -1,12 +1,26 @@
 import Navbar from "../Navbar/Navbar";
 import React from "react";
+import "./Home.css";
+import SideNav from "../SideNav/SideNav";
+import HeaderSearchPage from "../Search/Search";
 
 function Home (){
     
     return(
 <div>
-  
+ <div className="grid-container">
+<p>welcome</p>
+<p>hello</p>
+</div>
+   <div>
+    <img className="logo" src="https://seeklogo.com/images/D/drdo-logo-26087DC7CA-seeklogo.com.png"></img>
+    </div> 
+<div className="grid">
+    <p>welcome to user</p>
+</div>
 <Navbar/>
+<SideNav/>
+<HeaderSearchPage/>
 </div>
     )
 }
