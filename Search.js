79be85476d723c9305b726ed0f97b6ba9b0f9f53
nanditha98 <102796257+nanditha98@users.noreply.github.com>
2023-03-29 07:26:43 +0530

    Add files via upload

--- Search.js
+++ Search.js
@@ -1,13 +1,34 @@
 
-import React from 'react';
-import "./Search.css";
+// import React from 'react';
+// import "./Search.css";
 
-function Searchh() {
+// function Searchh() {
+//   return (
+//     <div className="searchBar">
+//       <input type="text" placeholder="Search" />
+//       <button type='submit'>search</button>
+//       </div>
+//   )
+// }
+// export default Searchh
+
+
+import React from "react";
+import { Form, FormControl,Button} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const SearchBar = () => {
   return (
-    <div className="searchBar">
-      <input type="text" placeholder="Search" />
-      <button type='submit'>search</button>
-      </div>
-  )
-}
-export default Searchh
+    <Form className="d-flex">
+      <FormControl
+        type="search"
+        placeholder="Search"
+        className="mr-2"
+        aria-label="Search"
+      />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  );
+};
+
+export default SearchBar;
