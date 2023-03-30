321f9c4ecb951eaaede80f5682b1a9d1df0dc0ea
nanditha98 <102796257+nanditha98@users.noreply.github.com>
2023-03-30 08:13:18 +0530

    Add files via upload

--- Search.js
+++ Search.js
@@ -1,34 +1,84 @@
 
-// import React from 'react';
-// import "./Search.css";
+// // import React from 'react';
+// // import "./Search.css";
 
-// function Searchh() {
+// // function Searchh() {
+// //   return (
+// //     <div className="searchBar">
+// //       <input type="text" placeholder="Search" />
+// //       <button type='submit'>search</button>
+// //       </div>
+// //   )
+// // }
+// // export default Searchh
+
+
+// import React from "react";
+// import { Form, FormControl,Button} from "react-bootstrap";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// const SearchBar = () => {
 //   return (
-//     <div className="searchBar">
-//       <input type="text" placeholder="Search" />
-//       <button type='submit'>search</button>
-//       </div>
-//   )
-// }
-// export default Searchh
+//     <Form className="d-flex">
+//       <FormControl
+//         type="search"
+//         placeholder="Search"
+//         className="mr-2"
+//         aria-label="Search"
+//       />
+//       <Button variant="outline-success">Search</Button>
+//     </Form>
+//   );
+// };
+
+// export default SearchBar;
 
 
-import React from "react";
-import { Form, FormControl,Button} from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 
-const SearchBar = () => {
+
+
+
+
+
+
+
+
+import React, { useState } from 'react';
+
+function SearchBar() {
+  const [ipAddress, setIPAddress] = useState('');
+  const [isValidIPAddress, setIsValidIPAddress] = useState(true);
+
+  function validateIPAddress(ipAddress) {
+    const pattern = /^([0-9]{1,3}\.){3}[0-9]{1,3}$/;
+    return pattern.test(ipAddress);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (validateIPAddress(ipAddress)) {
+      // The IP address is valid, so do something with it
+      setIsValidIPAddress(true);
+    } else {
+      // The IP address is not valid, so show an error message
+      setIsValidIPAddress(false);
+    }
+  }
+
+  function handleIPAddressChange(event) {
+    setIPAddress(event.target.value);
+  }
+
   return (
-    <Form className="d-flex">
-      <FormControl
-        type="search"
-        placeholder="Search"
-        className="mr-2"
-        aria-label="Search"
-      />
-      <Button variant="outline-success">Search</Button>
-    </Form>
+    <form onSubmit={handleSubmit}>
+      <label>
+        IP Address:
+        <input type="text" value={ipAddress} onChange={handleIPAddressChange} />
+      </label>
+      <button type="submit">Submit</button>
+      {!isValidIPAddress && <div className="error-message">Please enter a valid IP address</div>}
+    </form>
   );
-};
+}
 
 export default SearchBar;
