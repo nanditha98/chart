c1c1a0ffc75f11dea232f27362dfe8d2528c3e88
nanditha98 <102796257+nanditha98@users.noreply.github.com>
2023-04-05 07:34:28 +0530

    Add files via upload

--- Navbar.js
+++ Navbar.js
@@ -82,39 +82,30 @@
 // export default Nav
 
 
-import { useState, useEffect } from 'react';
+import React from "react";
 
-function AutoLogout({ duration, onLogout }) {
-  const [timer, setTimer] = useState(null);
+function Nav () {
+  return(
 
-  useEffect(() => {
-    // Set up the timer when the component mounts
-    const timeoutId = setTimeout(() => {
-      onLogout();
-    }, duration);
-
-    // Save the timer ID so we can clear it when the component unmounts
-    setTimer(timeoutId);
-
-    // Clean up the timer when the component unmounts or duration changes
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [duration, onLogout, timer]);
-
-  return null;
-}
-
-// Example usage:
-function App() {
-  const handleLogout = () => {
-    // Your logout logic here
-  };
-
-  return (
     <div>
-      <h1>Your app here</h1>
-      <AutoLogout duration={60000} onLogout={handleLogout} />
-    </div>
-  );
+      <div class="flex flex-col p-2 py-6 m-h-screen">
+        <div class="bg-white items-center justify-between w-full flex rounded-full shadow-lg p-2 mb-5  top-[10px] min-w-[40%] max-w-md mx-auto" >
+            <div>
+                <div class="p-2 mr-1 rounded-full hover:bg-gray-100 cursor-pointer">
+                    <svg class="h-6  w-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z" clip-rule="evenodd" />
+                    </svg>
+                </div>
+            </div>
+            <input class="font-bold uppercase rounded-full min-w-[75%] py-4 pl-4 text-gray-700 bg-gray-100 leading-tight focus:outline-none focus:shadow-outline lg:text-sm text-xs" type="text" placeholder="Search"/>
+            <div class="bg-gray-600 p-2 hover:bg-blue-400 cursor-pointer mx-2 rounded-full">
+                <svg class="w-6 h-6 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                    </svg>
+                </div>
+        </div>
+      </div>     
+  </div>
+  )
 }
+export default Nav;
