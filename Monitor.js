dc4c1a5b548c3640990252b795daa8f7fa01ae58
nanditha98 <102796257+nanditha98@users.noreply.github.com>
2023-04-20 19:10:31 +0530

    Add files via upload

--- Monitor.js
+++ Monitor.js
@@ -1,58 +1,46 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import SideNav from "../Main/SideNav/SideNav";
+import SideNav from "../Main/SideNav/SideNav"
 
 
 function Reg (){
     return(
+<section className="vh-100" style={{background: "#eee",height:"600px"}}>
+  <div className="container h-100">
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col-lg-12 col-xl-10">
+        <div className="card text-black" style={{borderRadius:"25px"}}>
+          <div className="card-body p-md-5" style={{background:"white",height:"600px"}}>
+            <div className="row justify-content-center">
+              <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
-<section class="h-100 gradient-form" style={{background: "#eee"}}>
-  <div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-xl-10">
-        <div class="card rounded-3 text-black">
-          <div class="row g-0">
-            <div class="col-lg-6">
-              <div class="card-body p-md-5 mx-md-4">
+                <p className="text-center h1 fw-bold mb-4 mx-1 mx-md-4 mt-4">Sign up</p>
 
-                <div class="text-center">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                    style={{width: "185px"}} alt="logo"/>
-                  <h4 class="mt-1 mb-5 pb-1">We are The Lotus Team</h4>
-                </div>
+                <form className="mx-1 mx-md-4">
 
-                <form>
-                  <p>Please login to your account</p>
-
-                  <div class="form-outline mb-4">
-                    <input type="email" id="form2Example11" class="form-control"
-                      placeholder="Phone number or email address" />
-                    <label class="form-label" for="form2Example11">Username</label>
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+                    <div className="form-outline flex-fill mb-0">
+                      <input type="text" id="form3Example1c" class="form-control" />
+                      <label className="form-label" for="form3Example1c">keyword</label>
+                    </div>
                   </div>
 
-                  <div class="form-outline mb-4">
-                    <input type="password" id="form2Example22" class="form-control" />
-                    <label class="form-label" for="form2Example22">Password</label>
+                  <div className="d-flex flex-row align-items-center mb-4">
+                    <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                    <div className="form-outline flex-fill mb-0">
+                      <input type="email" id="form3Example3c" class="form-control" />
+                      <label className="form-label" for="form3Example3c">username</label>
+                    </div>
                   </div>
-
-                  <div class="text-center pt-1 mb-5 pb-1">
-                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
-                      in</button>
-                    <a class="text-muted" href="#!">Forgot password?</a>
-                  </div>
-
-                  <div class="d-flex align-items-center justify-content-center pb-4">
-                    <p class="mb-0 me-2">Don't have an account?</p>
-                    <button type="button" class="btn btn-outline-danger">Create new</button>
+                  <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                    <button type="button" className="btn btn-primary btn-lg">Register</button>
                   </div>
 
                 </form>
 
               </div>
-            </div>
-            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
-              <div class="text-white px-3 py-4 p-md-5 mx-md-4">
-                <h4 class="mb-4">We are more than just a company</h4>
+              <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
                 <SideNav/>
               </div>
             </div>
@@ -61,7 +49,7 @@ function Reg (){
       </div>
     </div>
   </div>
-</section>
+</section> 
     )
 
 }
