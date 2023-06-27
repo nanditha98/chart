942dcf99ac6ac1e3d38dfe9b3dc66654052b7b29
nanditha98 <102796257+nanditha98@users.noreply.github.com>
2023-06-27 23:57:23 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -171,60 +171,6 @@
 
 
 
-// import React, { useEffect } from "react";
-// import "./App.css";
-
-// // Importing TagCloud package
-// import TagCloud from "TagCloud";
-
-// const App = () => {
-//   // Animation settings for Text Cloud
-//   useEffect(() => {
-//     return () => {
-//       const container = ".tagcloud";
-//       const texts = [
-//         "HTML",
-//         "CSS",
-//         "SASS",
-//         "JavaScript",
-//         "React",
-//         "Vue",
-//         "Nuxt",
-//         "NodeJS",
-//         "Babel",
-//         "Jquery",
-//         "ES6",
-//         "GIT",
-//         "GITHUB",
-//       ];
-
-//       const options = {
-//         radius: 300,
-//         maxSpeed: "normal",
-//         initSpeed: "normal",
-//         keep: true,
-//       };
-
-//       TagCloud(container, texts, options);
-//     };
-//   }, []);
-
-//   return (
-//     <>
-//       <div className="text-shpere">
-//         {/* span tag className must be "tagcloud"  */}
-//         <span className="tagcloud"></span>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default App;
-
-
-// npm i -S TagCloud
-
-
 
 
 // import React, { useState, useEffect } from 'react';
@@ -274,76 +220,171 @@
 
 
 
+// import "./App.css";
+// import { FiSearch } from "react-icons/fi";
+// import { useEffect, useRef, useState } from "react";
+// import axios from "axios";
+
+// const App = () => {
+//   // fetching data
+//   const [users, setusers] = useState([]);
+
+//   const fetchUsers = async () => {
+//     const response = await axios.get(
+//       "https://jsonplaceholder.typicode.com/users"
+//     );
+//     setusers(response.data);
+//   };
+//   useEffect(() => {
+//     fetchUsers();
+//   }, []);
+
+//   // filter users
+
+//   const [filtered, setfiltered] = useState([]);
+//   const [search, setsearch] = useState("");
+
+//   const searchRef = useRef();
+
+//   useEffect(() => {
+//     setfiltered(
+//       users.filter((item) =>
+//         item.name.toLowerCase().includes(search.toLowerCase())
+//       )
+//     );
+//   }, [search]);
+
+//   return (
+//     <div className="app">
+//       <div className="serach">
+//         <div className="searchbox">
+//           <input
+//             type="text"
+//             placeholder="search"
+//             onChange={(e) => setsearch(e.target.value)}
+//             ref={searchRef}
+//           />
+//           <FiSearch />
+//         </div>
+//         {search.length > 0 && (
+//           <div className="dropdown">
+//             {filtered.length > 0 ? (
+//               filtered.map((item, index) => {
+//                 return (
+//                   <div
+//                     className="card"
+//                     key={index}
+//                     onClick={(e) => {
+//                       searchRef.current.value = item.name;
+//                       setsearch("");
+//                     }}>
+//                     <p>{item.name}</p>
+//                   </div>
+//                 );
+//               })
+//             ) : (
+//               <p>no match</p>
+//             )}
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
 import "./App.css";
-import { FiSearch } from "react-icons/fi";
-import { useEffect, useRef, useState } from "react";
-import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactPaginate from "react-paginate";
+import { useEffect, useState } from "react";
 
-const App = () => {
-  // fetching data
-  const [users, setusers] = useState([]);
+function App() {
+  const [items, setItems] = useState([]);
 
-  const fetchUsers = async () => {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
+  const [pageCount, setpageCount] = useState(0);
+
+  let limit = 10;
+
+  useEffect(() => {
+    const getComments = async () => {
+      const res = await fetch(
+        // `http://localhost:3004/comments?_page=1&_limit=${limit}`
+        `https://jsonplaceholder.typicode.com/comments?_page=1&_limit=${limit}`
+      );
+      const data = await res.json();
+      const total = res.headers.get("x-total-count");
+      setpageCount(Math.ceil(total / limit));
+      // console.log(Math.ceil(total/12));
+      setItems(data);
+    };
+
+    getComments();
+  }, [limit]);
+
+  const fetchComments = async (currentPage) => {
+    const res = await fetch(
+      // `http://localhost:3004/comments?_page=${currentPage}&_limit=${limit}`
+      `https://jsonplaceholder.typicode.com/comments?_page=${currentPage}&_limit=${limit}`
     );
-    setusers(response.data);
+    const data = await res.json();
+    return data;
   };
-  useEffect(() => {
-    fetchUsers();
-  }, []);
 
-  // filter users
+  const handlePageClick = async (data) => {
+    console.log(data.selected);
 
-  const [filtered, setfiltered] = useState([]);
-  const [search, setsearch] = useState("");
+    let currentPage = data.selected + 1;
 
-  const searchRef = useRef();
+    const commentsFormServer = await fetchComments(currentPage);
 
-  useEffect(() => {
-    setfiltered(
-      users.filter((item) =>
-        item.name.toLowerCase().includes(search.toLowerCase())
-      )
-    );
-  }, [search]);
-
+    setItems(commentsFormServer);
+    // scroll to the top
+    window.scrollTo(0, 0)
+  };
   return (
-    <div className="app">
-      <div className="serach">
-        <div className="searchbox">
-          <input
-            type="text"
-            placeholder="search"
-            onChange={(e) => setsearch(e.target.value)}
-            ref={searchRef}
-          />
-          <FiSearch />
-        </div>
-        {search.length > 0 && (
-          <div className="dropdown">
-            {filtered.length > 0 ? (
-              filtered.map((item, index) => {
-                return (
-                  <div
-                    className="card"
-                    key={index}
-                    onClick={(e) => {
-                      searchRef.current.value = item.name;
-                      setsearch("");
-                    }}>
-                    <p>{item.name}</p>
-                  </div>
-                );
-              })
-            ) : (
-              <p>no match</p>
-            )}
-          </div>
-        )}
+    <div className="container">
+      <div className="row m-2">
+        {items.map((item) => {
+          return (
+            <div key={item.id} className="col-sm-6 col-md-4 v my-2">
+              <div className="card shadow-sm w-100" style={{ minHeight: 225 }}>
+                <div className="card-body">
+                  <h5 className="card-title text-center h2">Id :{item.id} </h5>
+                  <h6 className="card-subtitle mb-2 text-muted text-center">
+                    {item.email}
+                  </h6>
+                  <p className="card-text">{item.body}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
+
+      <ReactPaginate
+        previousLabel={"previous"}
+        nextLabel={"next"}
+        breakLabel={"..."}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination justify-content-center"}
+        pageClassName={"page-item"}
+        pageLinkClassName={"page-link"}
+        previousClassName={"page-item"}
+        previousLinkClassName={"page-link"}
+        nextClassName={"page-item"}
+        nextLinkClassName={"page-link"}
+        breakClassName={"page-item"}
+        breakLinkClassName={"page-link"}
+        activeClassName={"active"}
+      />
     </div>
   );
-};
+}
 
 export default App;
